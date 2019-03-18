2e97ecccf32ce52b688e9696593c05ca90878a99
Eric Czech <eric@hammerlab.org>
2019-03-18 15:20:59 -0400

    Incorporating representative cytokine and cell type sets, retagging and starting on more realistic labeling functions

--- pm_subtype_protein_relations/src/lib.py
+++ pm_subtype_protein_relations/src/lib.py
@@ -1,6 +1,7 @@
 import unicodedata
 import pandas as pd
 import os.path as osp
+from interlap import InterLap
 from env import META_DATA_DIR
 SPECIES_HUMAN_ID = 1
 SPECIES_MOUSE_ID = 2
@@ -15,3 +16,42 @@ def fix_jupyter_spacy_config():
 def get_entity_meta_data(table):
     path = osp.join(META_DATA_DIR, '{}.csv'.format(table))
     return pd.read_csv(path)
+
+
+class IntervalMergingDict(object):
+    """Dictionary for interval keys with configurable overlap merging"""
+    
+    def __init__(self, merge_fn):
+        self._intervals = InterLap()
+        self._result = {}
+        self.merge_fn = merge_fn
+        
+    def add(self, start, end, data=None):
+        # Add the new interval and associated data
+        self._intervals.add((start, end, data))
+        
+        # Pull a list of all intervals that overlap (including itself)
+        vals = list(self._intervals.find((start, end)))
+        assert len(vals) > 0, 'Expecting at least one interval result'
+        
+        # Merge intervals if there are more than one
+        res = vals[0] if len(vals) == 1 else self.merge_fn(vals)
+        if not isinstance(res, tuple) or len(res) != 3:
+            raise ValueError('Merged results should be 3-item tuples, not {}'.format(res))
+            
+        # Delete all overlapping ranges in final result before adding merged value
+        for e in vals:
+            if e[:2] in self._result:
+                del self._result[e[:2]]
+        self._result[res[:2]] = res[2]
+            
+    def keys(self):
+        return self._result.keys()
+    
+    def values(self):
+        return self._result.values()
+    
+    def items(self):
+        return zip(self.keys(), self.values())
+        
+        
